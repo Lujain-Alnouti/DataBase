61d3b7acc90571525ca858b3db9cce17676bedb7
Lujain-Alnouti <115452443+Lujain-Alnouti@users.noreply.github.com>
2022-12-14 20:34:34 +0300

    Add files via upload

---
+++ task_14_12/cust_table.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="cust_table.aspx.cs" Inherits="task_14_12.cust_table" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Customer_Name" runat="server" Text="Customer Name"></asp:Label>
+            <asp:TextBox ID="Cust_Name" runat="server"></asp:TextBox>
+            <br />
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+            <br />
+            <asp:Button ID="save" runat="server" Text="Save" OnClick="save_Click" />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+            <br /><br />
+            <asp:Image ID="Image1" runat="server" />
+            <br /><br />
+            <asp:Label ID="Label2" runat="server" Text=""></asp:Label>
+            
+        </div>
+    </form>
+</body>
+</html>
---
+++ task_14_12/cust_table.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.IO;
+//using System.Drawing;
+
+namespace task_14_12
+{
+    public partial class cust_table : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void save_Click(object sender, EventArgs e)
+             {
+            string folderPath = Server.MapPath("~/Files/");
+
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            string image_URL= "~/Files/" + Path.GetFileName(FileUpload1.FileName);
+            FileUpload1.SaveAs(folderPath + Path.GetFileName(FileUpload1.FileName));
+            SqlConnection connect = new SqlConnection("data source = DESKTOP-L7EOP8B\\SQLEXPRESS; database = task_14_12 ; integrated security=SSPI");
+            connect.Open();
+            string add_value = "insert into Customers values('"+ Cust_Name.Text+"','"+ image_URL+"') ";
+            SqlCommand cmd = new SqlCommand(add_value, connect);
+            cmd.ExecuteNonQuery();
+            connect.Close();
+            Label1.Text = "data inserted successfully";
+            Getdatab();
+        }
+
+        private void Getdatab()
+        {
+            Label1.Text = "";
+            SqlConnection connect2 = new SqlConnection("data source = DESKTOP-L7EOP8B\\SQLEXPRESS; database = task_14_12 ; integrated security=SSPI");
+            SqlCommand comand = new SqlCommand("select * from Customers", connect2);
+            connect2.Open();
+
+            SqlDataReader reada = comand.ExecuteReader();
+            //string image_URL = "~/Files/" + reada[2];
+            string table = "<table class='table table-striped'> <tr><th>ID</th> <th>Customer_Name</th> <th>Image_Url</th>";
+
+            while (reada.Read())
+            {
+                string image_URL =(string)reada[2];
+                table += $"<tr><td>{reada[0]}</td><td>{reada[1]}</td><td>{reada[2]}</td></tr>";
+                //Image1.ImageUrl = image_URL;
+                Image img = new Image();
+                this.Controls.Add(img);
+                img.ImageUrl = image_URL;
+                img.Height=150;
+                img.Width = 150;
+
+
+
+            }
+            table += "</table>";
+            Label2.Text = table;
+            connect2.Close();
+        }
+    }
+}
